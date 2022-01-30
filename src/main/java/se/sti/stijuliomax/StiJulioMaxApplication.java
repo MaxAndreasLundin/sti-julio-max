8ae69944574869331d7bfb4aa41a3cbcc09aeac0
Max Lundin <lundin-89@hotmail.com>
2022-01-30 11:38:00 +0100

    Initial commit

---
+++ src/main/java/se/sti/stijuliomax/StiJulioMaxApplication.java
@@ -0,0 +1,13 @@
+package se.sti.stijuliomax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StiJulioMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StiJulioMaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/sti/stijuliomax/StiJulioMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package se.sti.stijuliomax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StiJulioMaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
